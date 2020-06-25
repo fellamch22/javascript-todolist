8660333cbc59b11f6a917e5370e9ca880fd3a1c3
fella mechouar <hf_mechouar@esi.dz>
2020-06-26 01:10:02 +0200

    adding functions and listeners to control buttons delete / check

--- app.js
+++ app.js
@@ -5,16 +5,65 @@ const todoList = document.querySelector('.todo-list');
 
 //event listeners
 
-todoButton.addEventListener('click',addTodo)
+todoButton.addEventListener('click',addTodo);
+todoList.addEventListener("click",deleteCheck);
+
+
 
 // functions
 function addTodo(event){
     // preventing form from submitting
     event.preventDefault();
     //todo DIV
-    const todoDiv = document.createElement('div');
+    const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     // todo li
-    const newtodo = document.createElement('li');
-    newtodo.innerText
+    const newtodo = document.createElement("li");
+    newtodo.innerText =todoInput.value;
+    newtodo.classList.add("todo-item");
+    todoDiv.appendChild(newtodo);
+
+    //check mark button
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fas fa-check" ></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //check trash button
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class = "fas fa-trash" ></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // append to todo list
+
+    todoList.appendChild(todoDiv);
+    //clear todo input value
+
+    todoInput.value ="";
+    
+
+}
+
+function deleteCheck(e){
+    const item = e.target ;
+    // DELETE Button
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement ;
+        todo.classList.add("fall");
+        todo.addEventListener('transitioned',function(){
+        
+            todo.remove();
+
+        });
+    }
+
+    // check button
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement ;
+        todo.classList.toggle("completed");
+    }
+
 }
